4eb4ff694bd937529360600f96d727141e35b4fd
hedonicadapter <mailservice.samherman@gmail.com>
2024-12-14 20:11:04 +0100

    new colors

--- flake.nix
+++ flake.nix
@@ -11,22 +11,22 @@
     ...
   }: let
     colors = {
-      base00 = "#E3EFEF";
-      base01 = "#C9DBDC";
-      base02 = "#B0C5C8";
-      base03 = "#98AFB5";
-      base04 = "#8299A1";
-      base05 = "#6D828E";
-      base06 = "#5A6D7A";
-      base07 = "#485867";
-      base08 = "#b38686";
-      base09 = "#d8bba2";
-      base0A = "#aab386";
-      base0B = "#87b386";
-      base0C = "#86b3b3";
-      base0D = "#868cb3";
-      base0E = "#b386b2";
-      base0F = "#b39f9f";
+      base00 = "#DBDBDB";
+      base01 = "#E4E4E4";
+      base02 = "#C0C0C0";
+      base03 = "#4E4E4E";
+      base04 = "#1C1C1C";
+      base05 = "#232323";
+      base06 = "#1C1C1C";
+      base07 = "#E4E4E4";
+      base08 = "#CC5450";
+      base09 = "#A64270";
+      base0A = "#307878";
+      base0B = "#71983B";
+      base0C = "#C57D42";
+      base0D = "#376388";
+      base0E = "#D7AB54";
+      base0F = "#6D6D6D";
     };
 
     sanitizeColor = color:
